d015ddd518e7d3d126a1506487192d5b9156229d
Eswar Divi <76403422+EswarDivi@users.noreply.github.com>
2024-03-06 22:47:02 +0530

    updated db

--- Talkwithpdf.py
+++ Talkwithpdf.py
@@ -77,8 +77,7 @@ def PDF_loader(document):
     texts = text_splitter.split_documents(documents)
 
     # Create a new instance of Chroma with a unique persist_directory for each file
-    db = Chroma(persist_directory=f"./tempfolder/db_{os.path.basename(document).split('.')[0]}", embedding_function=embeddings)
-    db.persist(texts)
+    db = Chroma.from_documents(documents=texts,persist_directory=f"./tempfolder/db_{os.path.basename(document).split('.')[0]}", embedding_function=embeddings)
     retriever = db.as_retriever()
 
     qa = RetrievalQA.from_chain_type(
