27b5ce868c6802ffc864883b7da6a681af8f94e5
Eswar Divi <eswar.divi.902@gmail.com>
2024-04-17 08:47:52 +0000

    Updated New API Changes - User key required

--- Talkwithpdf.py
+++ Talkwithpdf.py
@@ -1,28 +1,31 @@
-# Import Required Libraries
-__import__("pysqlite3")
-import sys
-sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import os
+import sys
 import base64
-import streamlit as st
-from langchain.document_loaders import OnlinePDFLoader
-from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
-from langchain.embeddings import CohereEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_cohere import CohereEmbeddings
+from langchain_cohere import ChatCohere
+from langchain_cohere import CohereRerank
+from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
 from langchain.prompts import PromptTemplate
-from langchain.llms import Cohere
+from langchain_chroma import Chroma
+from langchain.chains import (
+    RetrievalQA,
+    ConversationalRetrievalChain,
+    StuffDocumentsChain,
+    LLMChain,
+)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import streamlit as st
 from datetime import datetime
 
-# Setting Up Streamlit Page
 st.set_page_config(page_title="Chat With PDF", page_icon=":smile:")
 
-# Creating Temp Folder
 if not os.path.exists("./tempfolder"):
     os.makedirs("./tempfolder")
 
-# tabs
-tab1, tab2, tab3 = st.tabs(["📈 Chat Here", "🗃 Relevant Chunks", "📄 PDF Preview"])
+tab1, tab2, tab3 = st.tabs(
+    ["💬 Chat with PDF", "📚 Relevant Document Chunks", "💾 Current Database in Memory"]
+)
 
 tab1.markdown(
     """
@@ -31,7 +34,7 @@ tab1.markdown(
     unsafe_allow_html=True,
 )
 
-# Saving Upload file to tempfolder
+
 def save_uploadedfile(uploadedfile):
     with open(
         os.path.join("tempfolder", uploadedfile.name),
@@ -40,62 +43,125 @@ def save_uploadedfile(uploadedfile):
         f.write(uploadedfile.getbuffer())
     return st.sidebar.success("Saved File")
 
-# Creating Sidebar for Utilites
+
 with st.sidebar:
-    st.title("Upload PDF")
-    st.write("For any Queries, please feel free to contact")
-    st.write("Email: [eswar.divi.902@gmail.com](mailto:eswar.divi.902@gmail.com)")
-    st.write("GitHub: [github.com/EswarDivi](https://github.com/EswarDivi)")
-    st.write("<h4>Powered by Cohere</h4>", unsafe_allow_html=True)
-    st.write("<p>For uninterrupted usage, visit the <a href='https://huggingface.co/spaces/eswardivi/ChatwithPdf' target='_blank'>HuggingFace Space</a></p>", unsafe_allow_html=True)
+    st.markdown(
+        "<h2 style='text-align: center; color: #007BFF;'>Upload PDF</h2>",
+        unsafe_allow_html=True,
+    )
+    cohere_api_key = st.text_input("Enter your Cohere API key", type="password")
+    if cohere_api_key:
+        try:
+            test_model = ChatCohere(model="command", cohere_api_key=cohere_api_key)
+            response = test_model.invoke("Hello")
+            if "error" in response:
+                raise Exception("Invalid API key")
+        except Exception as e:
+            st.error(f"Error: {str(e)} - Please enter a correct Cohere API key.")
+            st.stop()
+    else:
+        st.error("Please enter your Cohere API key to proceed.")
+    with st.expander("Contact Information"):
+        st.markdown("For any queries, please feel free to contact:")
+        st.markdown(
+            "Email: [eswar.divi.902@gmail.com](mailto:eswar.divi.902@gmail.com)"
+        )
+        st.markdown("GitHub: [github.com/EswarDivi](https://github.com/EswarDivi)")
+
+    with st.expander("Additional Information"):
+        st.info("Get Your API key at https://dashboard.cohere.com/api-keys")
+        st.markdown(
+            "<h4 style='text-align: center;'>Powered by Cohere</h4>",
+            unsafe_allow_html=True,
+        )
+
     uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
-    temp_r = st.slider("Temperature", 0.1, 0.9, 0.45, 0.1)
-    chunksize = st.slider("Chunk Size for Splitting Document ", 256, 1024, 400, 10)
-    clear_button = st.button("Clear Conversation", key="clear")
 
-# Initialzing Text Splitter
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunksize, chunk_overlap=10, separators=[" ", ",", "\n"])
+    with st.expander("Adjust Settings"):
+        temp_r = st.slider("Temperature", 0.1, 0.9, 0.45, 0.1)
+        chunksize = st.slider("Chunk Size for Splitting Document", 256, 1024, 400, 10)
+        clear_button = st.button("Clear Conversation", key="clear")
 
-# Intializing Cohere Embdedding
-embeddings = CohereEmbeddings(model="large", cohere_api_key=st.secrets["cohere_apikey"])
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=50, separators=[" ", ",", "\n"]
+)
+
+if cohere_api_key:
+    embeddings = CohereEmbeddings(
+        model="embed-english-v3.0",
+        cohere_api_key=cohere_api_key,
+    )
+    model_cohere = ChatCohere(
+        model="command",
+        cohere_api_key=cohere_api_key,
+    )
+
 
 def PDF_loader(document):
-    loader = OnlinePDFLoader(document)
-    documents = loader.load()
-    prompt_template = """
-    System Prompt:
-    Your are an AI chatbot that helps users chat with PDF documents. How may I help you today?
+    db_path = f"./tempfolder/db_{os.path.basename(document).split('.')[0]}"
+    if os.path.exists(db_path):
+        print("Using Cached One")
+        db_chroma = Chroma(embedding_function=embeddings, persist_directory=db_path)
+        retriever = db_chroma.as_retriever()
+    else:
+        loader = PyPDFLoader(document)
+        pages = loader.load_and_split()
+        docs = text_splitter.split_documents(pages)
+        db_chroma = Chroma.from_documents(
+            docs,
+            embeddings,
+            persist_directory=db_path,
+        )
+        retriever = db_chroma.as_retriever()
+
+    compressor = CohereRerank(cohere_api_key=cohere_api_key)
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=compressor, base_retriever=retriever
+    )
+
+    prompt_template = """You are an AI chatbot that helps users chat with PDF documents.
+    Use the following pieces of context to answer the question at the end. Please follow the following rules:
+    1. If you find the answer, write the answer in a Elegant way and add the list of sources that are **directly** used to derive the answer. Exclude the sources that are irrelevant to the final answer.
+    Example:
+    The Answer is derived from[1] this page
+    [1] Source_ Page:PageNumber
 
     {context}
 
-    {question}
-    """
-    PROMPT = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
+    Question: {question}
+    Helpful Answer:"""
+
+    QA_CHAIN_PROMPT = PromptTemplate.from_template(prompt_template)
+    document_prompt = PromptTemplate(
+        input_variables=["page_content", "source"],
+        template="Context:\ncontent:{page_content}\nPageNumber:{page}\nsource:{source}",
     )
-    chain_type_kwargs = {"prompt": PROMPT}
-    texts = text_splitter.split_documents(documents)
+    llm_chain = LLMChain(
+        llm=model_cohere, prompt=QA_CHAIN_PROMPT, callbacks=None, verbose=False
+    )
 
-    # Create a new instance of Chroma with a unique persist_directory for each file
-    db = Chroma.from_documents(texts,embeddings,persist_directory=f"./tempfolder/db_{os.path.basename(document).split('.')[0]}")
-    retriever = db.as_retriever()
+    combine_documents_chain = StuffDocumentsChain(
+        llm_chain=llm_chain,
+        document_variable_name="context",
+        document_prompt=document_prompt,
+        callbacks=None,
+        verbose=False,
+    )
 
-    qa = RetrievalQA.from_chain_type(
-        llm=Cohere(
-            model="command-xlarge-nightly",
-            temperature=temp_r,
-            cohere_api_key=st.secrets["cohere_apikey"],
-        ),
-        chain_type="stuff",
-        retriever=retriever,
+    qa = RetrievalQA(
+        combine_documents_chain=combine_documents_chain,
+        callbacks=None,
+        verbose=False,
+        retriever=compression_retriever,
         return_source_documents=True,
-        chain_type_kwargs=chain_type_kwargs,
     )
+
     return qa
 
-if uploaded_file is not None:
+
+if uploaded_file is not None and cohere_api_key:
     save_uploadedfile(uploaded_file)
-    file_size = os.path.getsize(f"tempfolder/{uploaded_file.name}") / (1024 * 1024)  # Size in MB
+    file_size = os.path.getsize(f"tempfolder/{uploaded_file.name}") / (1024 * 1024)
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"[{current_time}] Uploaded PDF: {file_size} MB")
     qa = PDF_loader("tempfolder/" + uploaded_file.name)
@@ -106,8 +172,9 @@ if uploaded_file is not None:
         unsafe_allow_html=True,
     )
 
+
 def generate_response(query, qa):
-    result = qa({"query": query, "chat_history": ""})
+    result = qa.invoke({"query": query, "chat_history": ""})
 
     tab2.markdown(
         "<h3 style='text-align: center;'>Relevant Documents Metadata</h3>",
@@ -118,7 +185,7 @@ def generate_response(query, qa):
     result["result"] = result["result"]
     return result["result"]
 
-# Session State
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -140,30 +207,41 @@ if prompt := st.chat_input("What is up?"):
                     Output = generate_response(prompt, qa)
                     full_response = Output if Output else "Failed to get the response."
                 fr = ""
-                # Convert the response to a string if needed
                 full_response = str(full_response)
                 for i in full_response:
                     import time
+
                     time.sleep(0.02)
                     fr += i
                     message_placeholder.write(fr + "▌")
                 message_placeholder.write(f"{full_response}")
-            st.session_state.messages.append({"role": "assistant", "content": full_response})
+            st.session_state.messages.append(
+                {"role": "assistant", "content": full_response}
+            )
     else:
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
-            message_placeholder.write('Please go ahead and upload the PDF in the sidebar, it would be great to have it there.')
-        st.session_state.messages.append({"role": "assistant", "content": "Please go ahead and upload the PDF in the sidebar, it would be great to have it there."})
+            message_placeholder.write(
+                "Please go ahead and upload the PDF in the sidebar, it would be great to have it there."
+            )
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": "Please go ahead and upload the PDF in the sidebar, it would be great to have it there.",
+            }
+        )
 
-# Enabling Clear button
 if clear_button:
     st.session_state.messages = []
 
-if uploaded_file is not None:
-    with tab3:
-        # file_path = f"tempfolder/{uploaded_file.name}"
-        # with open(file_path, "rb") as file:
-        #     base64_pdf = base64.b64encode(file.read()).decode("utf-8")
-        #     pdf_display = f"""<embed src="data:application/pdf;base64,{base64_pdf}" type="application/pdf" style="width:100%; height:100vh;"></embed>"""
-        #     st.markdown(pdf_display, unsafe_allow_html=True)
-        st.markdown("Coming Soon...")
+with tab3:
+    files = os.listdir("./tempfolder")
+    file_data = [
+        {
+            "File Name": file,
+            "Size (MB)": os.path.getsize(f"./tempfolder/{file}") / (1024 * 1024),
+        }
+        for file in files
+    ]
+    st.write("### Current Database Files in Memory")
+    st.table(file_data)
